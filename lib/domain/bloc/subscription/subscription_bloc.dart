d1fec95cda63ef71d36e8ae0a627417a0d703fea
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-07-31 12:46:20 +0300

    Дописала блок для подписки

--- lib/domain/bloc/subscription/subscription_bloc.dart
+++ lib/domain/bloc/subscription/subscription_bloc.dart
@@ -1,10 +1,13 @@
 
+import 'dart:async';
+
 import 'package:firebase_user_notes/domain/bloc/subscription/subscription_event.dart';
 import 'package:firebase_user_notes/domain/bloc/subscription/subscription_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../interactor/subscription_interactor.dart';
+import '../../model/subscription_model.dart';
 
 @Injectable()
 
@@ -15,5 +18,27 @@ class SubscriptionBloc extends Bloc<SubscriptionEvent, SubscriptionState> {
     on<SubscribeEvent>(_onSubscribeEvent);
     on<CheckSubscriptionEvent>(_onCheckSubscriptionEvent);
     add(CheckSubscriptionEvent());
+  }
+
+  FutureOr<void> _onSubscribeEvent(SubscribeEvent event, Emitter<SubscriptionState> emit) async {
+    try {
+      await _interactor.subscribe();
+    } catch (e) {
+      emit(SubscriptionErrorState());
+    }
+  }
+
+  FutureOr<void> _onCheckSubscriptionEvent(CheckSubscriptionEvent event, Emitter<SubscriptionState> emit) async {
+    emit(const LoadingSubscriptionState());
+    try {
+      SubscriptionModel subscription = await _interactor.checkSubscriptionActive();
+      if (subscription.deadline > 0) {
+        emit(ActiveSubscriptionState(subscription: subscription));
+      } else {
+        emit(const InactiveSubscriptionState());
+      }
+    } catch (e) {
+      emit(SubscriptionErrorState());
+    }
   }
 }
